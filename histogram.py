faf366b9f50412e3e80b2211d41245f1b64e5ccd
Veto1234-ii <vetoshnikova14@bk.ru>
2020-07-25 11:37:57 +0300

    From for to numpy

--- histogram.py
+++ histogram.py
@@ -11,7 +11,12 @@ import gc
 
 def threshold_selection(np_folder, n_band, info, N, Count):
     T = np.load(np_folder + r'Temperature_Band_' + str(n_band) +'_'+info + '.npy')
-    hist = histogram_calculation(T, N)
+    # res = histogram_calculation(T, N)
+    res = histogram_calculation_numpy(T, N)
+    hist = np.array(res[0][0])
+    step = res[1]
+    minval = res[2]
+    
     np.save(np_folder+'Histogram_'+info, hist)    
     print('histogram done')
     
@@ -39,8 +44,16 @@ def histogram_calculation(snapshot, N):
             t = snapshot[i][j]
             idx = int(round((t - minval)/step))
             hist[idx]+=1
-    return hist
+    return (hist, step,  minval)
 
+def histogram_calculation_numpy(snapshot, N):
+    minval = np.min(snapshot)
+    maxval = np.max(snapshot)
+    step = (maxval - minval)/(N-1)
+    fsnapshot = snapshot.ravel()
+    h1 = np.histogram(fsnapshot, N)
+    return (h1, step, minval)
 
-
+# a = threshold_selection('result/', 10, '143052_20140324_20170424', 550, 444)
+# print(a, ' threshold')
 
